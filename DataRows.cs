49db056eb7b6d700d49240c22dc56423f16c7a31
Segger87 <Segger87@gmail.com>
2018-04-04 11:43:57 +0100

    Initial commit

---
+++ DataRows.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportBank
+{
+    class DataRows
+    {
+        public string Date;
+        public string FromName;
+        public string ToName;
+        public string Narrative;
+        public string MoneyOwed;
+
+        public DataRows(string Date, string FromName, string ToName, string Narrative, string MoneyOwed)
+        {
+            //'this' is an instance of the class that we are in (so this calls the Date class above not the argument)
+            this.Date = Date;
+            this.FromName = FromName;
+            this.ToName = ToName;
+            this.Narrative = Narrative;
+            this.MoneyOwed = MoneyOwed;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadCsv();
+
+        }
+
+        public static void LoadCsv() 
+        {
+           
+
+            using (var reader = new StreamReader(@"C:\Work\Training\SupportBank\Transactions2014.csv"))
+            {
+                List<DataRows> dataRow = new List<DataRows>();
+
+                while (!reader.EndOfStream)
+                {
+                    var values = reader.ReadLine().Split(',');
+                  
+                    DataRows myRows = new DataRows(values[0], values[1], values[2], values[3], values[4]);
+
+                    dataRow.Add(myRows);
+
+                    Console.WriteLine();
+                }
+
+                foreach (var data in dataRow)
+                {
+                    Console.WriteLine("{0} {1} {2} {3} {4}",data.Date, data.FromName, data.ToName, data.Narrative, data.MoneyOwed);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
